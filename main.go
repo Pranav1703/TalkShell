127ea8cec80eaa8af8ce1c0180ee50da4c033d54
Pranav1703 <pranavsai2005@gmail.com>
2024-08-01 09:17:32 +0530

    fixes

--- main.go
+++ main.go
@@ -12,26 +12,21 @@ import (
 
 func main(){
 	
-
 	fmt.Println("CHOOSE: 1.server or 2.client")
+
 	reader := bufio.NewReader(os.Stdin)
 	choice,_ := reader.ReadString('\n')
 	choice = strings.TrimSpace(choice)
 
 	switch choice {
 		case "1":
-			
 			server.StartServer()
-				
-	
 			
 		case "2":
-
 			client.StartClient()
 			
-			
 		default:
-			fmt.Println("Choose between server or client")
+			fmt.Println("Choose between server(1) or client(2)")
 	}
 
 
